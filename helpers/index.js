2b69a665dbd85c26262b3d65ac28b6b90dd06ea7
valeri143 <valeria23200523251605@gmail.com>
2023-09-08 15:45:52 +0300

    hw06

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,9 @@
 const HttpError = require('./HttpError')
 const ctrlWrapper = require('./ctrlWrapper')
+const sendNodemailer = require('./sendNodemailer')
 
 module.exports = {
     HttpError,
-    ctrlWrapper
+    ctrlWrapper,
+    sendNodemailer
 }
